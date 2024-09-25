2412a3ece5e0bfea912c46351fae013f3f27a582
Efosee <valeraparaskev@gmail.com>
2024-09-25 11:17:11 +0300

    add new section - carousel

---
+++ src/js/scripts.js
@@ -0,0 +1,13 @@
+const slider = tns({
+	container: '.corousel__inner',
+	items: 1,
+	slideBy: 'page',
+	controls: false,
+	nav: false
+});
+document.querySelector('.prev').addEventListener('click', function(){
+	slider.goTo('prev')
+});
+document.querySelector('.next').addEventListener('click', function(){
+	slider.goTo('next')
+});
